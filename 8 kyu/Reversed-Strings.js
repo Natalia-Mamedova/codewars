ff35f8fa3ef68ba4c8a3b3223ff7246f89d38f4f
Natalia-Mamedova <natalialis462@gmail.com>
2025-07-11 12:57:04 +0300

    Add files via upload

---
+++ 8 kyu/Reversed-Strings.js
@@ -0,0 +1,9 @@
+// Description:
+// Complete the solution so that it reverses the string passed into it.
+// 'world'  =>  'dlrow'
+
+// Solution:
+function solution(str) {
+    return str.split('').reverse().join('');
+}
+console.log(solution('open'));
