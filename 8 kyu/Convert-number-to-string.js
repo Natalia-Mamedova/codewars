5e5917c4df7826e2b97c3d1283f1a634aff55ab3
Natalia-Mamedova <natalialis462@gmail.com>
2025-07-11 13:13:18 +0300

    Add files via upload

---
+++ 8 kyu/Convert-number-to-string.js
@@ -0,0 +1,8 @@
+// Description:
+// We need a function that can transform a number (integer) into a string.
+
+// Solution:
+function numberToString(num) {
+return "" + num
+}
+console.log(numberToString(99));
